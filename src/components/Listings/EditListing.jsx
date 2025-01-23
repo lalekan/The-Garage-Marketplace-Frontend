829df6d52ffbee84f368b71dc3deead564c0b2f7
lalekan <lekan.suleiman@gmail.com>
2025-01-22 23:17:27 -0800

    fixed image uploads

--- src/components/Listings/EditListing.jsx
+++ src/components/Listings/EditListing.jsx
@@ -108,7 +108,7 @@ const EditListing = () => {
             {existingImages.map((image, index) => (
               <img
                 key={index}
-                src={`https://the-garage-marketplace-fbea5251146d.herokuapp.com/api/${image}`}
+                src={`https://the-garage-marketplace-fbea5251146d.herokuapp.com/${image}`}
                 alt="Listing"
               />
             ))}
